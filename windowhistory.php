4508019dcd86792fe3575f7bbd288fec2a059e8a
doyeongkim <rlaehdud19@gmail.com>
2017-06-12 23:36:08 +0800

    Create windowhistory.php

--- windowhistory.php
+++ windowhistory.php
@@ -7,4 +7,12 @@ Version: 070617
 Author: Kim
 */
 
+function windowhistory_js_add() {
+   wp_register_script('windowhistory-script', plugins_url('/js/windowhistory.js', __FILE__));
+
+    wp_enqueue_script('windowhistory-script');
+}
+
+add_action("wp_enqueue_scripts", "windowhistory_js_add");
+
 ?>
