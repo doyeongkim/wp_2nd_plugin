8043879ea859cf965ef708c58c9b6c4e862fe3d6
doyeongkim <rlaehdud19@gmail.com>
2017-06-12 23:40:05 +0800

    Create scrolldown.php

--- scrolldown.php
+++ scrolldown.php
@@ -7,4 +7,12 @@ Version: 120617
 Author: Kim
 */
 
+function scrolldown_js_add() {
+   wp_register_script('scrolldown-script', plugins_url('/js/scrolldown.js', __FILE__));
+
+    wp_enqueue_script('scrolldown-script');
+}
+
+add_action("wp_enqueue_scripts", "scrolldown_js_add");
+
 ?>
